d78cdd1dc8a72c6941734d5d580fa4382601e109
byeongsu-hong <hong@byeongsu.dev>
2023-02-22 18:21:33 +0900

    move test utilities to module

--- test/LiquidityManager.t.sol
+++ test/LiquidityManager.t.sol
@@ -7,10 +7,8 @@ import {ERC20, WETH} from "@solmate/tokens/WETH.sol";
 import {LiquidityManager} from "../src/LiquidityManager.sol";
 import {Token, TokenPermit} from "../src/Types.sol";
 
-struct Account {
-    address addr;
-    uint256 key;
-}
+import {ERC2612} from "./ERC2612.sol";
+import {Account} from "./Test.sol";
 
 contract LiquidityManagerTest is Test {
     LiquidityManager internal lmgr;
@@ -19,34 +17,6 @@ contract LiquidityManagerTest is Test {
     WETH internal weth;
 
     Account internal depositor;
-
-    function permit(ERC20 _token, Account memory _permitter, address _spender, uint256 _value, uint256 _deadline)
-        internal
-        view
-        returns (bytes memory)
-    {
-        bytes32 digest = keccak256(
-            abi.encodePacked(
-                "\x19\x01",
-                _token.DOMAIN_SEPARATOR(),
-                keccak256(
-                    abi.encode(
-                        keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"),
-                        _permitter.addr,
-                        _spender,
-                        _value,
-                        _token.nonces(_permitter.addr),
-                        _deadline
-                    )
-                )
-            )
-        );
-
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(depositor.key, digest);
-        bytes memory signature = abi.encodePacked(r, s, v);
-
-        return signature;
-    }
 
     function newAccount(uint256 key) internal pure returns (Account memory) {
         return Account(vm.addr(key), key);
@@ -97,7 +67,7 @@ contract LiquidityManagerTest is Test {
                 address(weth),
                 0.5 ether,
                 block.timestamp + 1,
-                permit(weth, depositor, address(lmgr), 0.5 ether, block.timestamp + 1)
+                ERC2612.permit(vm, weth, depositor, address(lmgr), 0.5 ether, block.timestamp + 1)
             )
         );
 
@@ -107,7 +77,7 @@ contract LiquidityManagerTest is Test {
                 address(weth),
                 0.5 ether,
                 block.timestamp + 1,
-                permit(weth, depositor, address(lmgr), 0.5 ether, block.timestamp + 1)
+                ERC2612.permit(vm, weth, depositor, address(lmgr), 0.5 ether, block.timestamp + 1)
             )
         );
 
@@ -142,7 +112,7 @@ contract LiquidityManagerTest is Test {
                 address(weth),
                 1 ether,
                 block.timestamp + 1,
-                permit(weth, depositor, address(lmgr), 1 ether, block.timestamp + 1)
+                ERC2612.permit(vm, weth, depositor, address(lmgr), 1 ether, block.timestamp + 1)
             )
         );
         lmgr.withdraw(Token(address(weth), 0.5 ether));
