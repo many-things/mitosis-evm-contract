93785e9bebacc5126ab9ffb0c93e190385b120d0
hashableric <hashableric@gmail.com>
2023-03-01 22:31:45 +0900

    feat: add owned to DenomManager

--- src/DenomManager.sol
+++ src/DenomManager.sol
@@ -1,19 +1,21 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.17;
 
+import {Owned} from "@solmate/auth/Owned.sol";
+
 /**
  * @title DenomManager
  * @author @byeongsu-hong<hong@byeongsu.dev>
  * @notice This is denomination convertion middleware for Mitosis Gateway.
  * @dev There's a predefined addresses like NONE, ETH. be aware to use them.
  */
-contract DenomManager {
+contract DenomManager is Owned {
     address public constant NONE = address(0x0);
     address public constant ETH = address(0x1);
 
     mapping(address => address) public denoms;
 
-    constructor() {
+    constructor() Owned(msg.sender) {
         // predefined tokens
         denoms[address(0x0)] = NONE;
         denoms[address(0x1)] = ETH;
@@ -33,7 +35,7 @@ contract DenomManager {
      * @param _alias Mitosis denomination that correlates with target token
      * @dev TODO: add access control
      */
-    function addAlias(address _token, address _alias) public {
+    function addAlias(address _token, address _alias) public onlyOwner {
         denoms[_token] = _alias;
     }
 }
