195c3e7553faba25c046b2650930f2a23a4d2f07
byeongsu-hong <hong@byeongsu.dev>
2023-03-13 13:47:44 +0900

    script: empty deploy script

---
+++ script/deploy.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@std/Script.sol";
+
+import {DenomManager} from "@src/DenomManager.sol";
+import {Gateway} from '@src/Gateway.sol';
+import {LiquidityManager} from '@src/LiquidityManager.sol';
+
+contract Deploy is Script {
+    function setUp() public {}
+
+    function run() public {
+        string memory seedPhrase = vm.envString("MNEMONIC");
+        uint256 privateKey = vm.deriveKey(seedPhrase, 0);
+        address owner = vm.addr(privateKey);
+
+        vm.startBroadcast(privateKey);
+
+        // TODO: Deploy Something        
+
+        vm.stopBroadcast();
+    }
+}
